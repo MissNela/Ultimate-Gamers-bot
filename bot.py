750888489dc34ef7af30e069fcf0c2882e42e788
MissNela <44345612+MissNela@users.noreply.github.com>
2019-01-02 00:58:21 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,4 +35,4 @@ async def ping(ctx):
       t2 = time.perf_counter()
       await client.say("Ping: {}ms".format(round((t2-t1)*1000)))
 
-client.run(os.getenv("BOT-T")
+client.run(os.getenv("BOT_TOKEN")
