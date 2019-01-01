45ecb309c2612859e759cb9faf79d4601bf88011
MissNela <44345612+MissNela@users.noreply.github.com>
2019-01-02 00:43:59 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,33 @@
 
+import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import asyncio
+import colorsys
+import random
+import platform
+from discord import Game, Embed, Color, Status, ChannelType
+import os
+import functools
+import time
+import datetime
+import requests
+import json
+import praw
+import aiohttp
+
+client = commands.Bot(command_prefix = commands.when_mentioned_or("Ug!"))
+
+client.remove_command('help')
+
+
+@client.event
+async def on_ready():
+    print('Jsem tu :D')
+
+@client.command()
+async def help():
+  
+  
+  
+  
